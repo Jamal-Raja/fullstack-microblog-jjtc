07c35cbd00b32b3af8b74fb8fafade477b59b419
Jamal <jamalraja083@gmail.com>
2025-11-22 22:01:18 +0000

    test commit

--- server/server.js
+++ server/server.js
@@ -1,2 +1,31 @@
-console.log("hitt on server-side");
-console.log("this is a change");
+const sequelize = require("./src/config/database-config");
+
+const runServer = async () => {
+  try {
+    console.log("Attempting to connect to MySQL DB...");
+
+    await sequelize.authenticate();
+    console.log(
+      "\x1b[36m%s\x1b[0m",
+      "MySQL connection established successfully."
+    );
+
+    await sequelize.sync();
+    console.log("Models synced with DB.");
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(
+        "\x1b[1m\x1b[33m%s\x1b[0m",
+        `Server Running on --> http://localhost:${PORT}`
+      );
+    });
+  } catch (err) {
+    // HANDLE DB CONNECTION ERRORS
+    console.error("DB connection error -> ", err.message);
+    process.exit(1);
+  }
+};
+
+runServer();
+
