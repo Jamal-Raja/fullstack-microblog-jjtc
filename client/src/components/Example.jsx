bc0be52edfc9a822538be74284e4d5de822bcad6
Jimmy Lee <jimmylee88@users.noreply.github.com>
2025-11-25 21:16:22 +0000

    Hero component added
    Co-authored-by: Jamal-Raja <Jamal-Raja@users.noreply.github.com>

---
+++ client/src/components/Example.jsx
@@ -0,0 +1,11 @@
+function Examle() {
+  return (
+    <>
+      <div className="outline w-full bg-amber-100 h-screen">
+        THIS IS THE HERO COMPOENT
+      </div>
+    </>
+  );
+}
+
+export default Hero;
