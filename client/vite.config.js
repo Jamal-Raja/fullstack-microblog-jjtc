e5bfd367e8c14ac7a1f320739fb321eb60cebfef
Jamal <jamalraja083@gmail.com>
2025-11-28 20:16:21 +0000

    Add comments to vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Enable host for network access
+  // This allows the dev server to be accessed over the local network
   server: {
     host: true,
   },
