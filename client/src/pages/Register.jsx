3e0431a019fa74f42c952aa69e4edd017f8957b0
Jamal <jamalraja083@gmail.com>
2025-11-30 19:50:39 +0000

    Add Register functionality to register form.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,20 +1,72 @@
-import { useAuth } from "../context/AuthContext";
+// import { useAuth } from "../context/AuthContext";
+import axios from "axios";
 
 const Register = () => {
-  const { login } = useAuth();
+  // const { login } = useAuth();
 
-  const handleSubmit = (e) => {
+  // REGISTER USER
+  async function registerUser(e) {
     e.preventDefault();
-    login({ displayName: "" });
-  };
+    const form = new FormData(e.target);
+
+    const userInput = {
+      username: form.get("username"),
+      email: form.get("email"),
+      password: form.get("password"),
+      passwordConfirmation: form.get("passwordConfirmation"),
+    };
+    // login({ displayName: "" });
+
+    try {
+      const { data } = await axios({
+        method: "POST",
+        url: `${import.meta.env.VITE_API_BASE_URL}/api/users/register`,
+        data: userInput,
+      });
+
+      console.log("RESPONSE MSG -->", data.message);
+    } catch (error) {
+      console.error(error.response.data.message);
+    }
+  }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={registerUser}>
       <h2>Register</h2>
-      <input type="text" placeholder="Display Name" required />
-      <input type="email" placeholder="Email" required />
-      <input type="password" placeholder="Password" required />
-      <button type="submit">Register</button>
+      <input
+        name="username"
+        defaultValue="bob_marley123"
+        type="text"
+        placeholder="Username"
+        required
+      />
+      <input
+        name="email"
+        defaultValue="bob_marley123@gmail.com"
+        type="email"
+        placeholder="Email"
+        required
+      />
+      <input
+        name="password"
+        defaultValue="12345678"
+        type="password"
+        placeholder="Password"
+        required
+      />
+      <input
+        name="passwordConfirmation"
+        defaultValue="12345678"
+        type="password"
+        placeholder="Password Confirmation"
+        required
+      />
+      <button
+        type="submit"
+        className="bg-teal-500 px-4 py-1 rounded-full hover:scale-105 active:scale-95 cursor-pointer"
+      >
+        Register
+      </button>
     </form>
   );
 };
