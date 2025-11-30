2fb14fd8b33bac231c2ee63e9ea885d46fab116c
Jamal <jamalraja083@gmail.com>
2025-11-30 12:00:05 +0000

    Add links to sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,10 @@
 import { HomeIcon, MessageCircle, Search, User } from "lucide-react";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
   return (
     <>
-      {/* Sidebar */}
+      {/* SIDEBAR */}
       <aside className="w-64 border-r border-gray-200 p-4">
         <div className="flex items-center gap-2 mb-6">
           <input
@@ -12,23 +13,35 @@ function Sidebar() {
             className="w-full border border-gray-300 rounded-lg p-2 text-sm"
           />
         </div>
-
+        {/* SIDEBAR LINKS */}
         <nav className="space-y-5 text-md">
-          <div className="flex items-center gap-2 cursor-pointer font-semibold">
-            <HomeIcon />
-            <span>Home</span>
-          </div>
-          <div className="flex items-center gap-2 cursor-pointer">
-            <Search /> <span>Search</span>
-          </div>
-          <div className="flex items-center gap-2 cursor-pointer">
-            <User />
-            <span>Account</span>
-          </div>
-          <div className="flex items-center gap-2 cursor-pointer">
-            <MessageCircle />
-            <span>Messages</span>
-          </div>
+          {/* Home */}
+          <Link to="/">
+            <div className="flex items-center gap-2 cursor-pointer font-semibold">
+              <HomeIcon />
+              <span>Home</span>
+            </div>
+          </Link>
+          {/* Search */}
+          <Link to="">
+            <div className="flex items-center gap-2 cursor-pointer">
+              <Search /> <span>Search</span>
+            </div>
+          </Link>
+          {/* Account */}
+          <Link to="">
+            <div className="flex items-center gap-2 cursor-pointer">
+              <User />
+              <span>Account</span>
+            </div>
+          </Link>
+          {/* Messages */}
+          <Link to="/messages">
+            <div className="flex items-center gap-2 cursor-pointer">
+              <MessageCircle />
+              <span>Messages</span>
+            </div>
+          </Link>
         </nav>
       </aside>
     </>
