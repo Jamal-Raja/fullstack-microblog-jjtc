0e5166ba849e4d53bd3ff08c71fb13dd384dc859
Jamal <jamalraja083@gmail.com>
2025-12-01 20:38:30 +0000

    Add login functionality to login form. upon pressing Login, it returns the users token and userData

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,18 +1,54 @@
-import { useAuth } from "../context/AuthContext";
+// import { useAuth } from "../context/AuthContext";
+import axios from "axios";
 
 const Login = () => {
-  const { login } = useAuth();
+  // const { login } = useAuth();
 
-  const handleSubmit = (e) => {
+  // LOGIN USER
+  async function loginUser(e) {
     e.preventDefault();
-    login({ displayName: "" });
-  };
+    // login({ displayName: "" });
+
+    const form = new FormData(e.target);
+
+    const userInput = {
+      username: form.get("username"),
+      password: form.get("password"),
+    };
+
+    try {
+      const response = await axios({
+        method: "POST",
+        url: `${import.meta.env.VITE_API_BASE_URL}/api/users/login`,
+        data: userInput,
+      });
+      const token = response.data.accessToken;
+      const userData = response.data.data.userData;
+
+      console.log("TOKEN --> ", token);
+      console.log("userData --> ", userData);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={loginUser}>
       <h2>Login</h2>
-      <input type="email" placeholder="Email" required />
-      <input type="password" placeholder="Password" required />
+      <input
+        defaultValue="test"
+        name="username"
+        type="text"
+        placeholder="Username"
+        required
+      />
+      <input
+        defaultValue="12345678"
+        name="password"
+        type="password"
+        placeholder="Password"
+        required
+      />
       <button type="submit">Login</button>
     </form>
   );
