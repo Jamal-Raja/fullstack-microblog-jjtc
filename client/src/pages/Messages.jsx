bf650e70346dfea531b65b545f6e12ccb99b3a18
Jimmy Lee <jimmylee88@users.noreply.github.com>
2025-12-02 00:18:02 +0000

    Styling for Messages

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
 // Importing socket.io client
 import { io } from "socket.io-client";
-import {Send} from 'lucide-react';
+import { Send, MessageCircle } from "lucide-react";
 
 const Messages = () => {
   const [newMessage, setNewMessage] = useState("");
@@ -43,13 +43,20 @@ const Messages = () => {
 
         {/* Main content */}
         <main className="flex-1 p-6">
-          <h1 className="text-center text-teal-600 font-bold text-3xl">Messages</h1>
+          <div className="flex flex-row justify-center items-center gap-2 text-teal-600">
+            <MessageCircle size={30} />
+            <h1 className="text-center font-bold text-3xl">Messages</h1>
+          </div>
           <div className="rounded-2xl shadow-md p-8 border-gray-200">
-            <h1 className="font-bold p-3">All Messages</h1>
             {/* Messages list */}
-            <ul className="p-2 mb-2 [&>li:nth-child(odd)]:bg-gray-200">
+            <ul className="flex flex-col items-center p-2 mb-2 [&>li:nth-child(odd)]:bg-gray-100">
               {messages.map((msg, index) => (
-                <li key={index}>{msg}</li>
+                <li
+                  className="bg-gray-50 border border-neutral-100 p-3 mb-8 w-3/4 rounded-2xl rounded-tl-none shadow-md"
+                  key={index}
+                >
+                  {msg}
+                </li>
               ))}
             </ul>
 
@@ -59,20 +66,21 @@ const Messages = () => {
                 id="messageInput"
                 type="text"
                 autoComplete="off"
-                className="w-full bg-gray-100 px-4 rounded-2xl focus:outline-none focus:ring-2 focus:ring-teal-400 focus:border-teal-400 transition-all"
+                className="w-full bg-gray-100 px-4 rounded-full ring-1 ring-neutral-300 focus:bg-white focus:outline-none focus:ring-2 focus:ring-teal-400 focus:border-teal-400 transition-all"
                 placeholder="Enter your message here"
                 value={newMessage}
                 onChange={(e) => setNewMessage(e.target.value)}
               />
               {/* Send msg btn */}
               <button
+                type="submit"
                 onClick={(e) => {
                   e.preventDefault();
                   sendMsg();
                 }}
-                className="flex items-center justify-center p-3 text-white shadow-md hover:shadow-lg hover:scale-105 active:scale-95 cursor-pointer bg-teal-500 rounded-full"
+                className="flex items-center justify-center p-3 text-white shadow-md hover:shadow-lg hover:scale-105 active:scale-95 cursor-pointer bg-teal-500 rounded-full transition-all"
               >
-                <Send size={20}/>
+                <Send size={20} />
               </button>
             </form>
           </div>
